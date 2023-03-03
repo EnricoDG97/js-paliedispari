fb3e02597311e289e706c0bde4c47f9d404898f5
EnricoDG97 <enrico.delgaudio97@gmail.com>
2023-03-03 10:24:17 +0100

    feat: created files, started first exercise palindrome adding id elements and variables, added event listener

---
+++ script.js
@@ -0,0 +1,18 @@
+// Palidroma
+// Chiedere all’utente di inserire una parola
+// Creare una funzione per capire se la parola 
+// inserita è palindroma
+
+// seleziono gli id dal html e do una variabile
+let wordElement = document.getElementById("word");
+let inputButtonElement = document.getElementById("inputButton");
+
+// creazione variabile sentinella
+let isWordPalindrome = false;
+
+// aggiungo evento al bottone
+inputButtonElement.addEventListener("click", function() {
+
+
+    
+});
