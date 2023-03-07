1cb21be9c2484fa3dfa092ca44af36ab090f6008
EnricoDG97 <enrico.delgaudio97@gmail.com>
2023-03-07 18:19:34 +0100

    feat: finished second exercise

--- script.js
+++ script.js
@@ -4,8 +4,9 @@
 // inserita è palindroma
 
 // seleziono gli id dal html e do una variabile
-let wordElement = document.getElementById("word");
-let inputButtonElement = document.getElementById("inputButton");
+const wordElement = document.getElementById("word");
+const inputButtonElement = document.getElementById("inputButton");
+let outputPalindromeElement = document.getElementById("outputPalindrome");
 
 // creazione variabile sentinella
 let isWordPalindrome = true;
@@ -43,8 +44,51 @@ inputButtonElement.addEventListener("click", function() {
 
     if(isWordPalindrome) {
         console.log("è palindroma");
+        outputPalindromeElement.innerText = "E' una parola palindroma!";
     } else {
+        outputPalindromeElement.innerText = "Non è una parola palindroma!"
         console.log("non è palindroma");
     }
 
 }); 
+
+/*
+Pari e Dispari
+L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+Sommiamo i due numeri
+Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+Dichiariamo chi ha vinto.
+*/
+
+const numberSelectElement = document.getElementById("numberSelect").value;
+let inputUserNumber = Number(document.getElementById("inputNumber").value);
+const playButtonElement = document.getElementById("playButton");
+const resultOutputElement = document.getElementById("resultOutput");
+
+playButtonElement.addEventListener("click", function() {
+        let inputComputerNumber = Number(generateRandomNumber(1, 5));
+        resultOutputElement.innerHTML = "Hai giocato: " + inputUserNumber + "<br>" + "Il computer ha giocato: " + inputComputerNumber + "<br>";
+
+        if(isEvenOrOdd(inputUserNumber + inputComputerNumber) == numberSelectElement) {
+            resultOutputElement.innerHTML += "Hai vinto!";
+        }
+        else {
+            resultOutputElement.innerHTML += "Hai perso!";
+        }
+});
+
+
+// functions for second exercise
+function generateRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function isEvenOrOdd(number) {
+    if(number % 2 == 0) {
+        return "even";
+    }
+    else {
+        return "odd";
+    }
+}
